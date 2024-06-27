a6bc4f5fc57509633d39b154baad5144e788615a
Premraj Patil <ppremraj697@gmail.com>
2024-06-27 22:53:09 +0530

    Built complete Post Form Component and Implemented Next.js 14 Server Actions(1/2)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import PostForm from "@/components/PostForm";
 import UserInformation from "@/components/UserInformation";
 
 export default function Home() {
@@ -8,8 +9,7 @@ export default function Home() {
       </section>
 
       <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
-        <div className="bg-red-500">Hello</div>
-        {/* PostForm */}
+        <PostForm />
         {/* PostFeed */}
       </section>
 
